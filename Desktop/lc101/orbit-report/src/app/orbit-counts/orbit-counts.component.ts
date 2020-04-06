72bc27a48cd758998ac24cb3011a1953ed1ff52a
David <daviddong201712@gmail.com>
2020-04-06 14:00:57 -0500

    some bonus missions codes need modify

---
+++ Desktop/lc101/orbit-report/src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite'
+
+@Component({
+    selector: 'app-orbit-counts',
+    templateUrl: './orbit-counts.component.html',
+    styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+
+    @Input() satellites: Satellite[];
+    constructor() { }
+
+    ngOnInit() {
+    }
+    count(satellites: []) {
+      let result = 0;
+      for( let i = 0; i < satellites.length; i ++) {
+        if(satellites[i].type ) {
+          result += 1;
+        }
+      }
+    }
+}
