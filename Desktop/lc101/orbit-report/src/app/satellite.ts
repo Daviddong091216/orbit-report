1667a2fd3ceb277c21819ecf7ecd52e7305470c8
David <daviddong201712@gmail.com>
2020-04-06 11:35:50 -0500

    Highlight Space Debris

--- Desktop/lc101/orbit-report/src/app/satellite.ts
+++ Desktop/lc101/orbit-report/src/app/satellite.ts
@@ -11,8 +11,13 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+    shouldShowWarning() {
+        if((this.type).toLowerCase() === "space debris") {
+            return true;
+        }
+        return false;
+    }
 
 
-    
 }
 
